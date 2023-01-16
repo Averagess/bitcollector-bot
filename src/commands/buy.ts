03ef8038add308fe520ddac687aa95662a1f54b8
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-16 19:47:55 +0200

    formatted file, added new embed for result

--- src/commands/buy.ts
+++ src/commands/buy.ts
@@ -1,49 +1,111 @@
 import axios, { AxiosError } from "axios";
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 import { Player } from "../types";
+
+import intToString from "../utils/intToString";
 
 const buyCommand = {
   data: new SlashCommandBuilder()
     .setName("buy")
     .setDescription("Buys an item from the shop")
-    .addStringOption((option: any) => option.setName("item").setDescription("The item you want to buy").setRequired(true))
-    .addIntegerOption((option: any) => option.setName("amount").setDescription("The amount of items you want to buy")),
+    .addStringOption((option: any) =>
+      option
+        .setName("item")
+        .setDescription("The item you want to buy")
+        .setRequired(true)
+    )
+    .addIntegerOption((option: any) =>
+      option
+        .setName("amount")
+        .setDescription("The amount of items you want to buy")
+    ),
   async execute(interaction: ChatInputCommandInteraction) {
     try {
       const item = interaction.options.getString("item");
       const amount = interaction.options.getInteger("amount");
 
-      const { data } = await axios.post<Player>("http://localhost:3000/buyItem", {
-        discordId: interaction.user.id,
-        itemName: item,
-        amount: amount ? amount : 1,
-      });
+      const { data } = await axios.post<Player>(
+        "http://localhost:3000/buyItem",
+        {
+          discordId: interaction.user.id,
+          itemName: item,
+          amount: amount ? amount : 1,
+        }
+      );
 
-      const findMatchingItem = data.inventory.find((inventoryItem) => inventoryItem.name === item);
+      const findMatchingItem = data.inventory.find(
+        (inventoryItem) =>
+          inventoryItem.name.toLowerCase() === item?.toLowerCase()
+      );
 
-      if(amount && amount > 1 && findMatchingItem) {
-        await interaction.reply({ content: `You bought ${amount} ${item}'s, you now have ${findMatchingItem.amount} of them` });
-      }
-      else if (findMatchingItem) {
-        await interaction.reply({ content: `You bought an ${item}, you now have ${findMatchingItem.amount} of them` });
-      }
-      else {
-        await interaction.reply({ content: `You bought an ${item}` });
+      console.log(findMatchingItem);
+
+      const resultEmbed = new EmbedBuilder()
+        .setTitle("Purchase successful!")
+        .setColor("#a1fc03")
+        .setFooter({ text: `Requested by ${interaction.user.tag}` })
+        .setTimestamp();
+
+      if (amount && amount > 1 && findMatchingItem) {
+        resultEmbed.setDescription(
+          `You bought ${amount} ${findMatchingItem.name}'s, you now have ${findMatchingItem.amount} of them`
+        );
+        await interaction.reply({ embeds: [resultEmbed] });
+      } else if (findMatchingItem) {
+        resultEmbed.setDescription(
+          `You bought an ${findMatchingItem.name}, you now have ${findMatchingItem.amount} of them`
+        );
+        await interaction.reply({ embeds: [resultEmbed] });
+      } else {
+        resultEmbed.setDescription(`You bought an ${item}`);
+        await interaction.reply({ embeds: [resultEmbed] });
       }
     } catch (error) {
-      if(error instanceof AxiosError) {
-        if (error.response?.status === 404 && error.response?.data === "No such player") {
-          await interaction.reply({ content: "You don't have an account! Use /create to make one", ephemeral: true });
+      if (error instanceof AxiosError) {
+        if (
+          error.response?.status === 404 &&
+          error.response?.data === "No such player"
+        ) {
+          await interaction.reply({
+            content: "You don't have an account! Use /create to make one",
+            ephemeral: true,
+          });
+        } else if (error.response?.data === "No such item in the shop") {
+          await interaction.reply({
+            content: "No such item in the shop",
+            ephemeral: true,
+          });
+        } else if (error.response?.data.error === "not enough money") {
+          console.log(error.response.data);
+          const errorEmbed = new EmbedBuilder()
+            .setTitle("Purchase failed!")
+            .setColor("#fc0303")
+            .setDescription(
+              `You dont have enough bits! \nYou need: ${intToString(
+                error.response?.data.itemPrice
+              )} bits \nto buy ${intToString(error.response?.data.amount)} ${
+                error.response?.data.itemName
+              } and currently you have ${intToString(
+                error.response?.data.balance
+              )} bits.`
+            )
+            .setFooter({ text: `Requested by ${interaction.user.tag}` });
+          await interaction.reply({ embeds: [errorEmbed] });
         }
-        else if (error.response?.data === "No such item in the shop") {
-          await interaction.reply({ content: "No such item in the shop", ephemeral: true });
-        }
-        else if (error.response?.data === "Not enough money") {
-          await interaction.reply({ content: "Not enough money!", ephemeral: true });
-        }
+      } else {
+        console.error(`unknown error happened buying item: ${error}`);
+        await interaction.reply({
+          content:
+            "There was an error while buying the item... please try again later",
+          ephemeral: true,
+        });
       }
     }
-  }
-}
+  },
+};
 
 export default buyCommand;
