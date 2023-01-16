69395905b2deddc90ff8a7d088fe3ed5c4726492
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-17 00:42:06 +0200

    Created logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,21 @@
+import winston, { transports, format } from "winston";
+import "winston-daily-rotate-file";
+
+const logFormat = format.printf(({ level, message, timestamp }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`)
+const logger = winston.createLogger({
+    level: 'info',
+    format: format.combine(format.timestamp({format: new Date().toLocaleString()}), logFormat),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new transports.Console(),
+      new transports.DailyRotateFile({
+        filename: 'logs/%DATE%.log',
+        datePattern: 'DD-MM-YYYY',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      })
+    ]
+});
+
+export default logger;
