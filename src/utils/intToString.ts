d24686f2435f80b0b5c8018c82d4765638badafb
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-16 19:51:22 +0200

    created intToString formatter

---
+++ src/utils/intToString.ts
@@ -0,0 +1,29 @@
+function intToString(num : string) {
+  const newnum = num.toString().replace(/[^0-9.]/g, '');
+  try {
+    const bignum = BigInt(newnum);
+    if (bignum < 1000) {
+        return num;
+    }
+    let si = [
+      {v: 1E3, s: "K"},
+      {v: 1E6, s: "M"},
+      {v: 1E9, s: "B"},
+      {v: 1E12, s: "T"},
+      {v: 1E15, s: "P"},
+      {v: 1E18, s: "E"}
+      ];
+    let index;
+    for (index = si.length - 1; index > 0; index--) {
+        if (bignum >= si[index].v) {
+            break;
+        }
+    }
+    return ((bignum / BigInt(si[index].v)).toString().replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[index].s);    
+  } catch (error) {
+    return num.toString()
+  }
+}
+
+
+export default intToString;
