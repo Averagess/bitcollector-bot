4da2c97fee58b56de51cb3666f6e87242f23f8d2
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-17 17:55:45 +0200

    Added interaction defer, Added success embed

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,5 +1,6 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import axios, { AxiosError } from "axios";
+import ErrorEmbed from "../utils/ErrorEmbed";
 
 const createCommand = {
   data: new SlashCommandBuilder()
@@ -8,6 +9,8 @@ const createCommand = {
 
   async execute(interaction: ChatInputCommandInteraction) {
     try {
+      await interaction.deferReply();
+
       const { status } = await axios.post(
         "http://localhost:3000/initPlayer",
         {
@@ -16,15 +19,23 @@ const createCommand = {
         }
       );
       if (status === 200) {
-        await interaction.reply({ content: "Account created successfully!" });
+        const successEmbed = new EmbedBuilder()
+          .setTitle("Account created successfully!")
+          .setDescription("Next step is to use /buy to buy your first item!")
+          .setColor("#a1fc03")
+          .setFooter({ text: `Requested by ${interaction.user.tag}` })
+          .setTimestamp();
+        await interaction.editReply({ embeds: [successEmbed] });
       }
     } catch (error) {
       if (error instanceof AxiosError) {
         if(!error.response) throw new Error("No response from server");
         else if(error.response.status === 409){
-          await interaction.reply({ content: "You already have an account!", ephemeral: true });
+          const errorEmbed = ErrorEmbed({ title: "Account creation cancelled!", description: "You already have an account!", interaction: interaction })
+          await interaction.editReply({ embeds: [errorEmbed] });
         }
-        }
+        else throw new Error(`Uknown axios error happened creating an account.. Reason: ${error}`)
+      }
       else {
         throw new Error(`Unknown error happened creating an account.. Reason: ${error}`)
       }
