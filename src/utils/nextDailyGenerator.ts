3a07d8d670673f8a5c1539a9f3c4cb733bd0b6c5
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-21 19:58:58 +0200

    created nextdailyGenerator helper func

---
+++ src/utils/nextDailyGenerator.ts
@@ -0,0 +1,21 @@
+
+
+
+const nextDailyStringGenerator = (lastDailyRedeem: Date): string => {
+  const now = Date.now()
+
+  const hours = 24 - Math.round((now - lastDailyRedeem.getTime()) / 1000 / 60 / 60)
+  const minutes = (24*60) - Math.round((now - lastDailyRedeem.getTime()) / 1000 / 60)
+
+  console.log(hours)
+  console.log(minutes)
+
+  if(hours <= 0 && minutes <= 0) return "now"
+  else if(minutes === 1) return `in ${minutes} minute`
+  else if(hours <= 0 && minutes > 0) return `in ${minutes} minutes`
+  else if(hours === 1 && minutes <= 60) return `in ${hours} hour`
+  else if(hours > 1) return `in ${hours} hours`
+  else return "now"
+};
+
+export default nextDailyStringGenerator
