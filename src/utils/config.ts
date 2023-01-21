3fef3375380b896f7fd60a42f84904894941cbbe
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-21 12:49:24 +0200

    added warnings if backendurl or nodeenv is missing

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,6 +6,8 @@ import pkg from "../../package.json"
 if(!process.env.DISCORD_TOKEN) throw new Error("DISCORD_TOKEN is not set in .env")
 if(!process.env.GUILD_ID) throw new Error("GUILD_ID is not set in .env")
 if(!process.env.CLIENT_ID) throw new Error("CLIENT_ID is not set in .env")
+if(!process.env.BACKEND_URL) console.warn("BACKEND_URL is not set in .env, using localhost")
+if(!process.env.NODE_ENV) console.warn("NODE_ENV is not set in .env, using development")
 
 const config = {
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
