55dafa1c02e0b566ffe212007e0c4b3598d9acbb
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-22 20:17:57 +0200

    added a field for apikey

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,7 +6,7 @@ import pkg from "../../package.json"
 if(!process.env.DISCORD_TOKEN) throw new Error("DISCORD_TOKEN is not set in .env")
 if(!process.env.GUILD_ID) throw new Error("GUILD_ID is not set in .env")
 if(!process.env.CLIENT_ID) throw new Error("CLIENT_ID is not set in .env")
-
+if(!process.env.APIKEY) throw new Error("APIKEY is not set in .env")
 if(!process.env.BACKEND_URL) console.warn("BACKEND_URL is not set in .env, using http://localhost:3000")
 if(!process.env.NODE_ENV) console.warn("NODE_ENV is not set in .env, using development")
 
@@ -16,3 +16,4 @@ export const CLIENT_ID = process.env.CLIENT_ID
 export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000"
 export const NODE_ENV = process.env.NODE_ENV || "development"
 export const VERSION = process.env.npm_package_version || pkg.version
+export const APIKEY = process.env.APIKEY
