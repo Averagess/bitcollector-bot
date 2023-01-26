feede16ebaa9cb4cba75f19fbecf80023a3eef0e
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-27 00:38:32 +0200

    added new tips

--- src/resources/tips.ts
+++ src/resources/tips.ts
@@ -5,6 +5,8 @@ const tips: string[] = [
   "You can use `/buy [item] amount [num]` to buy multiple items at once!",
   "You can vote for the bot with `/vote` to get free coins and a random item!",
   "You can use `/daily` to get free coins every day!",
+  "You can use `/vote` to vote for the bot and get a crate as a reward!",
+  "You can open crates to get random items by using `/crate`!"
 ]
 
 export default tips
