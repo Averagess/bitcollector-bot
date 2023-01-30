8d9ec32cdac39af3e6ef07f3bd22ab1f07fe39b8
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-30 11:53:45 +0200

    removed unneeded return

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,14 +3,17 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { client } from "../client";
 import GenericSuccessEmbed from "../embeds/GenericSuccessEmbed";
 
-
 const helpCommand = {
   data: new SlashCommandBuilder()
     .setName("help")
     .setDescription("Replies with the commands you can use"),
   async execute(interaction: ChatInputCommandInteraction) {
-    if(!client.commands) return await interaction.reply("There are no commands to show")
-    const cmds = client.commands.map(cmd => ({ name: cmd.default.data.name, description: cmd.default.data.description}))
+    if (!client.commands)
+      return await interaction.reply("There are no commands to show");
+    const cmds = client.commands.map((cmd) => ({
+      name: cmd.default.data.name,
+      description: cmd.default.data.description,
+    }));
 
     const helpEmbed = GenericSuccessEmbed({ title: "Commands", interaction })
       .setDescription("The commands you can use")
@@ -20,10 +23,11 @@ const helpCommand = {
             name: `\`/${cmd.name}\``,
             value: cmd.description,
           };
-        }))
-    
-      return await interaction.reply({ embeds: [helpEmbed] });
-  }
-}
+        })
+      );
+
+    await interaction.reply({ embeds: [helpEmbed] });
+  },
+};
 
 export default helpCommand;
