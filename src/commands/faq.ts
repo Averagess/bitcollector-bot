6d452456586d11415f6d0d29d55d3292208ec870
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-02 11:07:47 +0200

    Updated FAQ.ts

--- src/commands/faq.ts
+++ src/commands/faq.ts
@@ -14,24 +14,25 @@ const faqCommand = {
       .addFields(
         {
           name: "What is this bot?",
-          value: `Bit Collector is a bot that allows you to collect an currency called bits,
-                  you can use these bits to buy upgrades that will increase your bits per second (CPS),
+          value: `Bit Collector is a bot that allows you to collect an currency called Bits,
+                  you can use these bits to buy items that will increase your bits per second (CPS),
                   with these bits you can compete with other players across the world.`,
         },
         {
           name: "How do I get started?",
           value: `To get started, send messages in chats on servers that the bot is in.
-                  You can also get bits by using the \`/daily\` command and by voting the bot.
+                  You can also get some bits by using the \`/daily\` command!
                   You will get bits for every message you send, and you can view
                   how many bits you have by using \`/balance\`.
-                  When your balance hits 50 bits, you can buy your first upgrade.
-                  You can check out the upgrades by using the \`/store\` command and then,
-                  you can buy upgrades by using the \`/buy\` command.`,
+                  When your balance hits 50 bits, you can buy your first item.
+                  You can check out the available items by using the \`/store\` command and then,
+                  you can buy items by using the \`/buy\` command.`,
         },
         {
           name: "How do I get more bits?",
-          value: `You can get more bits by sending messages in chats on servers that the bot is in,
-                  and you can also buy upgrades from the store with bits that will increase your bits per second (CPS) that increase your balance in the background.`,
+          value: `You can get more bits by sending messages in chats on servers that the bot is in for every message you get 1 Bit, you can also get daily bits by using the \`/daily\` command.
+                  you can also \`/vote\` for the bot, which grants you an crate, when you open this crate by using \`/crate\` you get some bits and a random item.
+                  when you have enough bits at the start, you can buy your first item from the \`/store\` and this item will give you bits on the background, even when you are offline.`,
         },
         {
           name: "I believe I have found a bug, what do I do?",
