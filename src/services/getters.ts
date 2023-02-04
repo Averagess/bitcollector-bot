805792e00e527d5d4c4e389349b6f7ebf39829d8
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-04 14:54:12 +0200

    created an getItems service

--- src/services/getters.ts
+++ src/services/getters.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { Leaderboard } from "../types";
+import { Leaderboard, Item } from "../types";
 import { APIKEY, BACKEND_URL } from "../utils/config";
 
 const headers = {
@@ -9,5 +9,10 @@ const headers = {
 
 export const getLeaderboard = async () => {
   const { data, status } = await axios.get<Leaderboard>(`${BACKEND_URL}/leaderboard`, { headers });
+  return { data, status };
+};
+
+export const getItems = async () => {
+  const { data, status } = await axios.get<Item[]>(`${BACKEND_URL}/allItems`, { headers });
   return { data, status };
 };
