4f69b17b8d2ba3b1f139a55e477f4850e1b4e828
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-15 20:04:04 +0200

    created index.ts

---
+++ src/embeds/index.ts
@@ -0,0 +1,9 @@
+import GenericSuccessEmbed from "./GenericSuccessEmbed";
+import GenericErrorEmbed from "./GenericErrorEmbed";
+import NoAccountEmbed from "./NoAccountEmbed";
+
+export {
+  GenericErrorEmbed,
+  GenericSuccessEmbed,
+  NoAccountEmbed
+};
