735a8d637a7d4f96639347f522ff97e607bca4d9
Averagess <45748163+Averagess@users.noreply.github.com>
2023-04-03 22:11:59 +0300

    Updated index.ts exports

--- src/embeds/index.ts
+++ src/embeds/index.ts
@@ -1,9 +1,11 @@
 import GenericSuccessEmbed from "./GenericSuccessEmbed";
 import GenericErrorEmbed from "./GenericErrorEmbed";
 import NoAccountEmbed from "./NoAccountEmbed";
+import ShopItemEmbed from "./ShopItemEmbed";
 
 export {
   GenericErrorEmbed,
   GenericSuccessEmbed,
-  NoAccountEmbed
+  NoAccountEmbed,
+  ShopItemEmbed
 };
